3561e2d719a7d6b4d070d95424f0dc833b1df41b
Shreyas <shreyasgandhi673@gmail.com>
2025-07-25 12:17:48 +0530

    added supervisor agent to fastapi router

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from .models import ChatModelPayload, ChatModel, ChatModelListItem
 from api.db import get_session
 from typing import List
+from api.ai.agents import get_supervisor
 from api.ai.services import generate_email_message
-from api.ai.schemas import EmailMessageSchema
+from api.ai.schemas import EmailMessageSchema, SupervisorMessageSchema
 router = APIRouter()
 
 # /api/chat
@@ -27,7 +28,11 @@ def chat_list_messages(session:Session = Depends(get_session)):
 # curl -X POST -d '{"message":"Give me the summary of why is it better to go outside"}' -H "Content-Type: application/json" http://localhost:8000/api/chat/
 # curl -X POST -d '{"message":"Give me the summary of why is it better to go outside"}' -H "Content-Type: application/json" https://ai-agent-docker-python.onrender.com/api/chat
 
-@router.post("/", response_model=EmailMessageSchema)
+# curl -X POST -d '{"message": "Research why it is good to go outside and email me the results"}' -H "Content-Type: application/json" http://localhost:8080/api/chat/
+# curl -X POST -d '{"message": "Research what is the best type of food in different parts of the world and email me the results"}' -H "Content-Type: application/json" https://ai-agent-docker-python.onrender.com/api/chat/
+
+
+@router.post("/", response_model=SupervisorMessageSchema)
 def chat_create_message(
     payload : ChatModelPayload,
     session : Session = Depends(get_session)
@@ -40,6 +45,22 @@ def chat_create_message(
     # ready to store in the database
     session.add(obj)
     session.commit()
+    supe = get_supervisor()
+    msg_data = {
+        "messages" : [
+           { 
+            "role" : "user",
+            "content" : f"{payload.message}"
+            },
+        ]
+    }
+    results = supe.invoke(msg_data)
+    if not results:
+        raise HTTPException(status_code=400,detail="Error with Supervisor")
+    
+    messages = results.get("messages")
+    if not messages:
+        raise HTTPException(status_code=400,detail="Error with Supervisor")
     # session.refresh(obj)   # ensures id/primary key added to the object instance
-    response = generate_email_message(payload.message)
-    return response
+
+    return messages[-1]
